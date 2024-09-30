1de52835d9adb7ce1e8a56e64f243a6bf1cc3b53
Tyler Boutwell <tylerboutwell1@yahoo.com>
2024-09-30 06:28:17 -0500

    Started project, made classes main, quiz, and questions_src

---
+++ main.py
@@ -0,0 +1,4 @@
+from questions_src import questions
+from quiz import run_quiz
+
+run_quiz(questions)
---
+++ quiz.py
@@ -0,0 +1,14 @@
+
+def run_quiz(question_list):
+        score = 0
+        for question in question_list:
+            print(question["question"])
+            for option in question["options"]:
+                print(option)
+            answer = input("Enter your answer: (A, B, or C): ").upper()
+            if answer == question["answer"]:
+                print("Correct!")
+                score += 1
+            else:
+                print("Incorrect!")
+        print(f"You got {score} correct out of {len(question_list)} questions.")
